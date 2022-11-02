d1abb1489b88628e58b3437b728820c49fd648de
samuelsleight <samuel.sleight@gmail.com>
2022-11-02 21:21:45 +0000

    ♻️ Use `cfg_attr` instead of a separate struct

--- src/level/assets.rs
+++ src/level/assets.rs
@@ -3,19 +3,15 @@ use bevy_asset_loader::prelude::*;
 
 use super::config::LevelConfig;
 
-#[cfg(target_family = "wasm")]
 #[derive(AssetCollection)]
 pub struct LevelAssets {
-    #[asset(
-        paths("levels/1.level", "levels/2.level", "levels/3.level"),
-        collection(typed)
+    #[cfg_attr(
+        target_family = "wasm",
+        asset(
+            paths("levels/1.level", "levels/2.level", "levels/3.level"),
+            collection(typed)
+        )
     )]
-    pub levels: Vec<Handle<LevelConfig>>,
-}
-
-#[cfg(not(target_family = "wasm"))]
-#[derive(AssetCollection)]
-pub struct LevelAssets {
-    #[asset(path = "levels", collection(typed))]
+    #[cfg_attr(not(target_family = "wasm"), asset(path = "levels", collection(typed)))]
     pub levels: Vec<Handle<LevelConfig>>,
 }
